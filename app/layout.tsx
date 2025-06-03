ca2e7a619bb411be26aa1e34fd6f4266a5ed1b3e
Om Sharma <mail.coderom@gmail.com>
2025-06-03 18:12:29 +0530

    chore: add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { inter, firaCode } from '../lib/fonts'
 import { baseMetadata, jsonLdSchema } from "@/lib/metadata";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = baseMetadata;
 
@@ -31,7 +21,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${firaCode.variable} antialiased`}
       >
         {children}
       </body>
